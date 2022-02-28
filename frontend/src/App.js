9cd56d3add0f6f233a4c3e1f976b1d2b8905a164
Petko Petkov <petko@petko.Petko.net>
2022-02-28 21:13:43 +0200

    last commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -44,7 +44,7 @@ const App = () => {
 	// fetch data from server
 	const fetchData = async (year, month) => {
 		const tempData = await fetch(
-			`https://temp-app-5ko.herokuapp.com/sender-info/show/${year}/${month}`
+			`https://localhost:8000/sender-info/show/${year}/${month}`
 		);
 
 		const jsonifiedData = await tempData.json();
--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use("/sender-info", router);
 const PORT = process.env.PORT || 5000;
 
 const checkConn = async () => {
-	return await mongoose.connect("mongodb+srv://users:uc0ZTIag9ie52t8R@temp-hum.jytno.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+	return await mongoose.connect(process.env.MONGO_URI);
 };
 
 const conn = checkConn();
